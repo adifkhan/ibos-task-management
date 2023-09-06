c6d1d8ebe0a9bda06da7f528d6b1aaa7720f883a
adifkhan <pkadif@gmail.com>
2023-09-06 21:44:11 +0600

    initialization

---
+++ src/pages/Register.js
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "../styles/register.module.css";
+
+const Register = () => {
+  return (
+    <section className={style.register__container}>
+      <div className={style.register__wrapper}>
+        <div className="heading">
+          <h2>Register</h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
