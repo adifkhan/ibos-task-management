bc8dfe2cd63b84f8af9c926ae08fd0478eea7b73
adifkhan <pkadif@gmail.com>
2023-09-08 15:56:36 +0600

    some issue solved

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -1,26 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { idb } from "../utilities/database";
 
 const useTasks = () => {
   const [tasksData, setTasksData] = useState([]);
 
-  const dbPromise = idb.open("task-manager", 1);
-  dbPromise.onsuccess = () => {
-    const db = dbPromise.result;
-    const transaction = db.transaction("taskCollection", "readonly");
-    const taskData = transaction.objectStore("taskCollection");
-    const tasks = taskData.getAll();
+  useEffect(() => {
+    const dbPromise = idb.open("task-manager", 1);
 
-    tasks.onsuccess = (query) => {
-      setTasksData(query.srcElement.result);
+    dbPromise.onerror = (event) => {
+      console.log("Couldn't open IndexedDB", event);
     };
-    tasks.onerror = (event) => {
-      console.log("Error occurred while geting Tasks");
+
+    dbPromise.onupgradeneeded = (event) => {
+      const db = dbPromise.result;
+      if (!db.objectStoreNames.contains("taskCollection")) {
+        db.createObjectStore("taskCollection", { keyPath: "taskId" });
+      }
     };
-    transaction.oncomplete = () => {
-      db.close();
+
+    dbPromise.onsuccess = () => {
+      const db = dbPromise.result;
+      const transaction = db.transaction("taskCollection", "readonly");
+      const taskData = transaction.objectStore("taskCollection");
+      const tasks = taskData.getAll();
+
+      tasks.onsuccess = (query) => {
+        setTasksData(query.srcElement.result);
+      };
+      tasks.onerror = (event) => {
+        console.log("Error occurred while geting Tasks");
+      };
+      transaction.oncomplete = () => {
+        db.close();
+      };
     };
-  };
+  }, []);
+
   return [tasksData];
 };
 
--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -1,26 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { idb } from "../utilities/database";
 
 const useUserData = () => {
   const [usersData, setUsersData] = useState([]);
 
-  const dbPromise = idb.open("task-manager", 1);
-  dbPromise.onsuccess = () => {
-    const db = dbPromise.result;
-    const transaction = db.transaction("userCollection", "readonly");
-    const userData = transaction.objectStore("userCollection");
-    const users = userData.getAll();
+  useEffect(() => {
+    const dbPromise = idb.open("task-manager", 1);
 
-    users.onsuccess = (query) => {
-      setUsersData(query.srcElement.result);
+    dbPromise.onerror = (event) => {
+      console.log("Couldn't open IndexedDB", event);
     };
-    users.onerror = (event) => {
-      console.log("Error occurred while geting users");
+
+    dbPromise.onupgradeneeded = (event) => {
+      const db = dbPromise.result;
+      if (!db.objectStoreNames.contains("userCollection")) {
+        db.createObjectStore("userCollection", { keyPath: "email" });
+      }
     };
-    transaction.oncomplete = () => {
-      db.close();
+
+    dbPromise.onsuccess = () => {
+      const db = dbPromise.result;
+      const transaction = db.transaction("userCollection", "readonly");
+      const userData = transaction.objectStore("userCollection");
+      const users = userData.getAll();
+
+      users.onsuccess = (query) => {
+        setUsersData(query.srcElement.result);
+      };
+      users.onerror = (event) => {
+        console.log("Error occurred while geting users");
+      };
+      transaction.oncomplete = () => {
+        db.close();
+      };
     };
-  };
+  }, []);
   return [usersData];
 };
 
